67c81a3d81e3df55e776034f01e58e672c53398b
jonny0808 <jonny0808@mail.ru>
2024-02-27 22:44:01 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,195 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "");
+
+    //-------------- 1 -----------------
+    //int* p;
+
+    //int* p1{}; // int* p1 = 0;
+
+
+    //-------------- 2.1 -----------------
+    //int x = 2;
+    //int* y = &x; // От любой переменной можно взять адрес при помощи операции взятия адреса "&". Эта операция возвращает указатель
+
+    //-------------- 2.2 -----------------
+    //int number{ 23 };
+    //int* pnumber{ &number };
+    //cout << "number = " << number << endl;
+    //cout <<"pnumber = "<< pnumber << endl;
+    //cout << "&pnumber = " << &pnumber << endl;
+
+    //-------------- 3 -----------------
+    //int number{ 25 };
+    //int* pnumber{ &number };
+    //cout << "Address = " << pnumber << endl;
+    //cout << "Value = " << *pnumber << endl;
+
+    //int n2{ *pnumber }; // n2 получает значение, которое хранится по адресу в pn1
+    //cout << "n2 = " << n2 << endl;  // n2=25
+
+    //*pnumber = 45;
+    //cout << "number = " << number << endl;
+
+    //-------------- 4.1 -----------------
+    //int n{ 10 };
+    //int* pn{ &n };
+    //cout << "address=" << pn << "\tvalue=" << *pn << endl;
+    //pn++;
+    //cout << "address=" << pn << "\tvalue=" << *pn << endl;
+    //pn--;
+    //cout << "address=" << pn << "\tvalue=" << *pn << endl;
+
+    //-------------- 4.2 -----------------
+    //double d{ 10.6 };
+    //double* pd{ &d };
+    //cout << "Pointer pd: address:" << pd << endl;
+    //pd = pd + 2;   // увеличение адреса на 16 байт - 2 объекта double
+    //cout << "Pointer pd: address:" << pd << endl;
+
+    //short n{ 5 };
+    //short* pn{ &n };
+    //cout << "Pointer pn: address:" << pn << endl;
+    //pn = pn - 3;   // уменьшение адреса на 6 байт - размер 3 объектов short
+    //cout << "Pointer pn: address:" << pn << endl;
+
+    //-------------- 4.3 -----------------
+    //int a{ 10 };
+    //int b{ 23 };
+    //int* pa{ &a };
+    //int* pb{ &b };
+    //auto ab{ pa - pb };
+
+    //cout << "pa: " << pa << endl;
+    //cout << "pb: " << pb << endl;
+    //cout << "ab: " << ab << endl;
+
+    //-------------- 5.1 -----------------
+    //int a{ 10 };
+    //int* pa{ &a };
+    //int b{ *pa + 20 };   
+    //pa++;               
+    //cout << "b: " << b << endl; 
+    //cout << "pa: " << pa << endl;
+
+    //-------------- 5.2 -----------------
+    //int a{ 10 };
+    //int* pa{ &a };
+    //cout << "pa: address=" << pa << "\tvalue=" << *pa << endl;
+    //int b{ ++ * pa }; 
+
+    //cout << "b: value=" << b << endl;
+    //cout << "pa: address=" << pa << "\tvalue=" << *pa << endl;
+
+    //int b{ *++pa };
+    //cout << "b: value=" << b << endl;
+    //cout << "pa: address=" << pa << "\tvalue=" << *pa << endl;
+
+    //-------------- 6 -----------------
+   // int number{ 5 };
+   // int& refNumber{ number };
+   // cout << "number = " << number << endl;
+   // cout << "refNumber = " << refNumber << endl;
+   //// int& refNumber = 10;  // ошибка!!!
+   // //refNumber = 20;
+   // //cout << "number = " << number << endl;
+
+    //-------------- 7.1 -----------------
+    //int numbers[]{ 1, 2, 3, 4, 5 };
+    //// меняем число на его квадрат
+    //for (auto n : numbers)
+    //{
+    //    n = n * n;
+    //}
+    //// смотрим результат
+    //for (auto n : numbers)
+    //{
+    //    cout << n << "\t";
+    //}
+    //cout << endl;
+
+    //-------------- 7.1 -----------------
+    //int numbers[]{ 1, 2, 3, 4, 5 };
+    //// теперь n - ссылка на элемент массива
+    //for (auto& n : numbers)
+    //{
+    //    n = n * n;
+    //}
+    //// смотрим результат
+    //for (auto n : numbers)
+    //{
+    //    cout << n << "\t";
+    //}
+    //cout << endl;
+
+    //-------------- 8 -----------------
+//int* numbers{ new int[4]{ 1, 2, 3, 4 } };
+//
+//// получение элементов через синтаксис массивов
+//std::cout << numbers[0] << std::endl;       // 1
+//std::cout << numbers[1] << std::endl;       // 2
+//
+//// получение элементов через операцию разыменования
+//std::cout << *numbers << std::endl;         // 1
+//std::cout << *(numbers + 1) << std::endl;     // 2
+
+//-------------- 9 -----------------
+//unsigned rows = 3;       // количество строк
+//unsigned columns = 2;    // количество столбцов
+//int** numbers{ new int* [rows] {} };  // выделяем память под двухмерный массив
+//// выделяем память для вложенных массивов
+//for (unsigned i{}; i < rows; i++)
+//{
+//    numbers[i] = new int[columns] {};
+//}
+//
+//// удаление массивов    
+//for (unsigned i{}; i < rows; i++)
+//{
+//    delete[] numbers[i];
+//}
+//delete[] numbers;
+
+//-------------- 10 -----------------
+    //typedef int I4[4];
+    //// или так
+    //using I4 = int[4];
+
+    //I4 a, b;
+
+
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,92 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "");
+     cout.setf(ios::fixed);
+    cout.precision(2);   
+    
+    const int n = 5;
+    double** matrix{ new double* [n] {} };
+
+    for (int i = 0; i < n; i++)
+    {
+        matrix[i] = new double[n + 1]{};
+        for (int j = 0; j < n + 1; j++)
+        {
+
+            matrix[i][j] = (double)(rand() * 100) / RAND_MAX;
+            cout << matrix[i][j] << '\t';
+        }
+        cout << endl;
+    }
+    cout << endl;
+    double a[n + 1], b[n + 1];
+    for (int i = 0; i < n + 1; i++)
+    {
+        a[i] = (double)(rand() * 100) / RAND_MAX;
+        cout << a[i] << '\t';
+    }
+    cout << endl;
+    for (int i = 0; i < n + 1; i++)
+    {
+        b[i] = (double)(rand() * 100) / RAND_MAX;
+        cout << b[i] << '\t';
+    }
+    cout << endl;
+    int p, q;
+    cin >> p >> q;
+
+    double** newMatrix{ new double* [n+1] {} };
+
+    for (int i = 0; i <n+1; i++)
+    {
+        newMatrix[i] = new double[n + 2]{};
+        if (i <= p)
+        {
+            for (int j = 0; j < n + 1; j++)
+            {
+                newMatrix[i][j] = matrix[i][j];
+                cout << newMatrix[i][j] << '\t';
+            }
+            cout << endl;
+        }
+        if (i == p + 1)
+        {
+            for (int j = 0; j < n + 1; j++)
+            {
+                newMatrix[p][j] = a[j];
+                cout << newMatrix[p][j] << '\t';
+            }
+            cout << endl;
+        }
+        if (i > p + 1)
+
+        {
+            for (int j = 0; j < n + 1; j++)
+            {
+                newMatrix[i][j] = matrix[i - 1][j];
+                cout << newMatrix[i][j] << '\t';
+            }
+            cout << endl;
+        }
+    }
+
+    for (unsigned i{}; i < n; i++)
+    {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    for (unsigned i{}; i < n+1; i++)
+    {
+        delete[] newMatrix[i];
+    }
+    delete[] newMatrix;
+
+}
+
+
